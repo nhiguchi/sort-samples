a3204d7a256559197dc492feab477ee2b23bcf08
nhiguchi <naoto.higuchi@bizreach.co.jp>
2020-05-05 23:47:07 +0900

    merge sort

--- src/main/scala/Sorter.scala
+++ src/main/scala/Sorter.scala
@@ -2,6 +2,10 @@ trait Sorter {
   def sort(unsortedSeq: Seq[Int]): Seq[Int]
 }
 
+object Sorters {
+  val seq = Seq(InsertionSorter, MergeSorter)
+}
+
 object InsertionSorter extends Sorter {
   override def sort(unsortedSeq: Seq[Int]): Seq[Int] = {
     unsortedSeq.foldLeft(Seq.empty[Int]) { (seq, e) =>
@@ -9,8 +13,28 @@ object InsertionSorter extends Sorter {
       if (index == -1) {
         seq :+ e
       } else {
-        (seq.take(index) :+ e) ++ seq.drop(index)
+        seq.take(index) :+ e :++ seq.drop(index)
       }
+    }
+  }
+}
+
+object MergeSorter extends Sorter {
+  override def sort(unsortedSeq: Seq[Int]): Seq[Int] = {
+    def _sort(leftSeq: Seq[Int], rightSeq: Seq[Int]): Seq[Int] = {
+      (leftSeq, rightSeq) match {
+        case (left, Nil)  => left
+        case (Nil, right) => right
+        case (leftHead :: leftTail, rightHead :: rightTail) =>
+          if (leftHead < rightHead) leftHead +: _sort(leftTail, rightSeq)
+          else rightHead +: _sort(leftSeq, rightTail)
+      }
+    }
+    val pivot = unsortedSeq.size / 2
+    if (pivot == 0) unsortedSeq
+    else {
+      val split = unsortedSeq.splitAt(pivot)
+      _sort(sort(split._1), sort(split._2))
     }
   }
 }
--- src/test/scala/SorterSpec.scala
+++ src/test/scala/SorterSpec.scala
@@ -1,12 +1,12 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class SorterSpec extends AnyFunSuite {
-
-  val UnsortedSeq = Seq(3, 6, 2, 7, 9, 1, 8, 5, 4)
-  val SortedSeq = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-  test("insertion sort") {
-    assertResult(SortedSeq)(InsertionSorter.sort(UnsortedSeq))
+  test("all sort") {
+    val unsortedSeq = Seq(3, 6, 2, 7, 1, 8, 5, 4)
+    val sortedSeq = Seq(1, 2, 3, 4, 5, 6, 7, 8)
+    Sorters.seq.foreach { sorter =>
+      println(s"sorter: ${sorter.getClass}")
+      assertResult(sortedSeq)(sorter.sort(unsortedSeq))
+    }
   }
-
 }
